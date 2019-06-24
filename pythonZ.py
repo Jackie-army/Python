8740214eef2610534fad3a5559dbf122c84b7403
Zhangbo <deyiangel1987@163.com>
2019-06-24 22:01:14 +0800

    Initial commit

---
+++ pythonZ.py
@@ -0,0 +1,31 @@
+import pymongo
+
+print('welcome to Myhome')
+test test test test tuple
+test test test test tuple
+test test test test tuple
+test test test test tuple
+test test test test tuple
+test test test test tuple
+test test test test tuple
+
+
+test test test test cisco
+test test test test cisco
+test test test test cisco
+test test test test cisco
+test test test test cisco
+test test test test cisco
+test test test test cisco
+test test test test cisco
+
+
+
+test test test test cisco
+test test test test cisco
+test test test test cisco
+test test test test cisco
+test test test test cisco
+test test test test cisco
+
+print('welcome to Myhome')
